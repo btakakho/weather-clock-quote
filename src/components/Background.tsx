f7b98fe0a817d637f89de095abd82bea3bd0cfe7
Bislan Takakho <b.takakho@gmail.com>
2022-04-26 22:59:00 +0300

    Import images with file loader

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,3 +1,11 @@
+import day from '/day.jpg'
+import night from '/night.jpg'
+import cloud from '/cloudy.jpg'
+import rain from '/rainy.jpg'
+import rainNight from '/rainy-night.jpg'
+import snow from '/snowy.jpg'
+import snowNight from '/snowy-night.jpg'
+
 export enum DayTimeEnum {
   Day,
   Night,
@@ -24,68 +32,68 @@ export enum WeatherEnum {
 
 export const themes = {
   [WeatherEnum.Clear]: {
-    [DayTimeEnum.Day]: '/day.jpg',
-    [DayTimeEnum.Night]: '/night.jpg',
+    [DayTimeEnum.Day]: day,
+    [DayTimeEnum.Night]: night,
   },
   [WeatherEnum.Clouds]: {
-    [DayTimeEnum.Day]: '/cloudy.jpg',
-    [DayTimeEnum.Night]: '/cloudy.jpg',
+    [DayTimeEnum.Day]: cloud,
+    [DayTimeEnum.Night]: cloud,
   },
   [WeatherEnum.Rain]: {
-    [DayTimeEnum.Day]: '/rainy.jpg',
-    [DayTimeEnum.Night]: '/rainy-night.jpg',
+    [DayTimeEnum.Day]: rain,
+    [DayTimeEnum.Night]: rainNight,
   },
   [WeatherEnum.Snow]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Alien]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Ash]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Drizzle]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Dust]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Fog]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Haze]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Mist]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Sand]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Smoke]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Squall]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Thunderstorm]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
   [WeatherEnum.Tornado]: {
-    [DayTimeEnum.Day]: '/snowy.jpg',
-    [DayTimeEnum.Night]: '/snowy-night.jpg',
+    [DayTimeEnum.Day]: snow,
+    [DayTimeEnum.Night]: snowNight,
   },
 } as Record<WeatherEnum, any>
 
